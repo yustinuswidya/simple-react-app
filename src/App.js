9a23eb4531c5c8130719880cbb75489cfb913a89
yustinuswidya <yustinus.widya@gmail.com>
2023-05-02 17:31:39 +0700

    rollback to component

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
             {/* This route is for home component 
             with exact path "/", in component props 
             we passes the imported component*/}
-            <Route exact path="/about" element={About} />
+            <Route exact path="/about" component={About} />
               
             {/* This route is for about component 
             with exact path "/about", in component 
