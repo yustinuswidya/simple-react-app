ffc2691781f95f7fb5807d6808fd0cd41667d8a6
yustinuswidya <yustinus.widya@gmail.com>
2023-05-13 13:40:09 +0700

    coba find dan findIndex

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -7,12 +7,18 @@ import {
 function Menubar(){
     const names = ['Home', 'About', 'Contact'];
 
-	var numbers = [3,16,2,48,5];
+	var numbers = [3,16,2,48,5,25];
 	const newNumber = numbers.find(function (num){
 		return num > 20;
 	});
 
 	console.log(newNumber);
+
+	const idx = numbers.findIndex(function (num){
+		return num > 20;
+	});
+
+	console.log(idx);
 
     return (
 	    <div>
