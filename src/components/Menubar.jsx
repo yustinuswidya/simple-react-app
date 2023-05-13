33f72400db8e76bed31a500bfc43701862db9912
yustinuswidya <yustinus.widya@gmail.com>
2023-05-13 13:36:12 +0700

    try find > 20

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -7,9 +7,9 @@ import {
 function Menubar(){
     const names = ['Home', 'About', 'Contact'];
 
-	var numbers = [3,56,2,48,5];
+	var numbers = [3,16,2,48,5];
 	const newNumber = numbers.find(function (num){
-		return num> 10;
+		return num > 20;
 	});
 
 	console.log(newNumber);
