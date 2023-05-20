4909973843b266c7328a5fc1888640c318aecc03
yustinuswidya <yustinus.widya@gmail.com>
2023-05-20 09:05:08 +0700

    typo bigcat

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home    from "./components/Home";
 import About   from "./components/About";
 import Contact from "./components/Contact";
 import Menubar from "./components/Menubar";
-import BigCat  from "./components/BigCat";
+import BigCat  from "./components/Bigcat";
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
         <Menubar />
-        <BigCat/>
+        <Bigcat/>
 
         {/* This is the alias of BrowserRouter i.e. Router */}
           <Routes>
