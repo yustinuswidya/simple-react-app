ac77fca5d184c1dd43acbef8266fca155d7ddb4d
yustinuswidya <yustinus.widya@gmail.com>
2023-05-20 15:22:20 +0700

    remove return

--- src/components/Bigcat.jsx
+++ src/components/Bigcat.jsx
@@ -33,11 +33,12 @@ function Bigcat(){
         <ol>
           {cats.map((cat) => {
             // console.log(cat);
-            return (
+            
               <div class="card" style="width: 18rem;">
-                <img src="https://randombig.cat/0b627ed7-ca93-400e-9d38-d0c4ab62f8b6.jpg" class="card-img-top"/>
+                <img src={cat} alt="https://randombig.cat/0b627ed7-ca93-400e-9d38-d0c4ab62f8b6.jpg" class="card-img-top"/>
               </div>
-          )})}
+            }
+          )}
         </ol>
       </div>  
     );
