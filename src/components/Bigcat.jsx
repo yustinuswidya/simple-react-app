f75dda4cf9169b5e864302c17360bbbfb678f833
yustinuswidya <yustinus.widya@gmail.com>
2023-05-20 15:18:38 +0700

    static src

--- src/components/Bigcat.jsx
+++ src/components/Bigcat.jsx
@@ -35,7 +35,7 @@ function Bigcat(){
             // console.log(cat);
             return (
               <div class="card" style="width: 18rem;">
-                <img src={$cat} alt="https://randombig.cat/0b627ed7-ca93-400e-9d38-d0c4ab62f8b6.jpg" class="card-img-top"/>
+                <img src="https://randombig.cat/0b627ed7-ca93-400e-9d38-d0c4ab62f8b6.jpg" class="card-img-top"/>
               </div>
           )})}
         </ol>
